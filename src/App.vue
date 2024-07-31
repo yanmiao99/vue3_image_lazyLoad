c8cb899f579c006acda99ab8f00c874f9e8a1987
yanmiao99 <503084944@qq.com>
2024-07-31 10:19:33 +0800

    feat : 更新注释

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
 
 <template>
   <div v-for="item in 100" :key="item">
+    <!-- 使用 v-lazeLoad 代替 src 即可 -->
     <img v-lazyLoad="`https://picsum.photos/500/400?v${item}`" />
   </div>
 </template>
--- src/lazyLoad.directive.js
+++ src/lazyLoad.directive.js
@@ -2,7 +2,7 @@ import defaultImage from "./assets/defaultImage.webp";
 
 export default {
   mounted(el, binding) {
-    console.log('binding========',binding);
+    // console.log('binding========',binding);
     const img = el;
     img.src = defaultImage; // 赋值默认图片
     const observer = new IntersectionObserver((entries) => {
@@ -12,9 +12,9 @@ export default {
         observer.unobserve(img);
       }
     }, {
-      root: null, // 默认为视窗
+      root: null, // 默认为视窗 , 如果需要定制, 建议从 binding 中传入
       rootMargin: '0px', // 根元素外边距
-      threshold: 0.5 // 交叉比例
+      threshold: 0.5 // 交叉比例 , 代表屏幕50%的时候就加载
     });
 
     observer.observe(img);
